00b0f8dacbe83238b85abfc235f3e1a2a07cf099
minhee71 <minhee71@cs104u-2e.acad.fullerton.edu>
2019-08-31 13:26:06 -0700

    Diamond pattern using cout and endl per 2.2 in lecure.

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-
+    //Using cout to output a string and endl output to a new line. Section 2.2 in lecture.
     std::cout << "    *" << std::endl;
     std::cout << "   ***" << std::endl;
     std::cout << "  *****" << std::endl;
