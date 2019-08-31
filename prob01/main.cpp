5516042f9f313b6de6a8fb97cfbb909fa2560832
minhee71 <minhee71@cs104u-2e.acad.fullerton.edu>
2019-08-31 13:22:21 -0700

    Diamond pattern using cout and endl

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,18 @@
-// Name: 
+// Name: Peter Choi
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+
+    std::cout << "    *" << std::endl;
+    std::cout << "   ***" << std::endl;
+    std::cout << "  *****" << std::endl;
+    std::cout << " *******" << std::endl;
+    std::cout << "  *****" << std::endl;
+    std::cout << "   ***" << std::endl;
+    std::cout << "    *" << std::endl;
 
   return 0;
 }
